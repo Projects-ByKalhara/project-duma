e4fd513d9c23f961d1259eea1ad2b579af7e93ea
Kalhara JA <kalharajak.21@uom.lk>
2023-07-23 20:40:09 +0530

    updated

--- src/components/WarningSection/WarningSection.js
+++ src/components/WarningSection/WarningSection.js
@@ -1,9 +1,25 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import './WarningSection.css'
 import { Container } from 'react-bootstrap'
+import { useState } from 'react';
+import { ref, onValue } from "firebase/database";
+import DB from '../../Database/Firebase';
 
 function WarningSection() {
+
+  let [average,setAverage] = useState();
+
+  useEffect(() => {
+
+    const averageRef = ref(DB,'Feed/average');
+    onValue(averageRef, (snapshot) => {
+      const data = snapshot.val();
+      setAverage(data);
+    });
+
+  }, []);
+
   return (
     <Container>
       <div className="warning-outer" >
@@ -11,7 +27,7 @@ function WarningSection() {
           Warnings
         </div>
         <div className="warning-content">
-          * No Warnings For Now
+          {average > 50 ? <div className="warning-text-good">The Pet is in Good Health</div> : <div className="warning-text-bad">The Pet is in Bad Health</div>}
         </div>
       </div>
     </Container>
